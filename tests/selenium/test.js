4e175e09906651471e6827cd8efa50086b8c0dd8
fnfuente <fnfuente@inf.utfsm.cl>
2024-07-10 10:30:25 -0400

    fix: selenium and jest integration for npm run test command

--- tests/selenium/test.js
+++ tests/selenium/test.js
@@ -1,38 +1,32 @@
-const {Builder, Browser, Key, until, By} = require('selenium-webdriver');
+const { Builder, Browser, Key, until, By } = require('selenium-webdriver');
 
-(async function helloSelenium() {
-  let driver = await new Builder().forBrowser(Browser.CHROME).build();
+describe('Selenium Tests', () => {
+    let driver;
 
-  try {
-    await driver.get('http://localhost:3000/');
+    beforeAll(async () => {
+        driver = await new Builder().forBrowser(Browser.CHROME).build();
+    });
 
+    afterAll(async () => {
+        if (driver) {
+            await driver.quit();
+        }
+    });
 
-    await testTitleMainPage(driver);
-    await testNavigateAndVerifyTitle(driver);
-    console.log('Test passed!');
+    test('Verify Main Page Title', async () => {
+        await driver.get('http://localhost:3000/');
+        await driver.wait(until.titleIs('Fashion Trend'), 5000);
+        console.log('Test 1 passed: Verify main page title');
+    });
 
-
-} finally {
-
-    await driver.quit();
-}
-
-})();
-
-async function testTitleMainPage(driver) {
-  await driver.wait(until.titleIs('Fashion Trend'), 5000);
-  console.log('Test 1 passed: Verify main pagetitle');
-}
-
-
-
-async function testNavigateAndVerifyTitle(driver) {
-  await driver.get('http://localhost:3000/');
-  await driver.wait(until.elementLocated(By.css('.px-8')), 5000);
-
-  await driver.findElement(By.css('.px-8')).click();
-  await driver.sleep(2000);
-  await driver.findElement(By.css('.lucide-user')).click();
-  await driver.sleep(2000);
-  console.log('Test 2 passed: Navigate and click Tienda button then Admin button');
-}
+    test('Navigate and Click Buttons', async () => {
+        await driver.get('http://localhost:3000/');
+        await driver.wait(until.elementLocated(By.css('.px-8')), 5000);
+        
+        await driver.findElement(By.css('.px-8')).click();
+        await driver.sleep(2000);
+        await driver.findElement(By.css('.lucide-user')).click();
+        await driver.sleep(2000);
+        console.log('Test 2 passed: Navigate and click buttons');
+    });
+});
